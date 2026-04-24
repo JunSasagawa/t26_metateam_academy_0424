62fe363ecbcd7ce6f0d8e957bc445a8fa25bb840
Tetsuro-Kitajima <tetsuro.kitajima@metateam.co.jp>
2026-04-24 12:28:44 +0900

    Trainig.Javaの実装

--- src/main/java/jp/co/metateam/training/Training.java
+++ src/main/java/jp/co/metateam/training/Training.java
@@ -3,9 +3,33 @@ package jp.co.metateam.training;
 public class Training {
     public static void main(String[] args) {
         // 問1
+    int pastweight = 10;
+    int N = 30;
+    int increase = N - pastweight;
+        System.out.println(increase);
 
         // 問2
-
+    int n = 50;
+    int m = 3;
+    int p =10;
+    
+    int remaining = n-(m*p);
+        System.out.println(remaining);
         // 問3
+    String[]words = {"book","pen","car","computer"};
+     int max = words[0].length();
+     int min = words[0].length();
+
+     for(int i = 0; i <words.length; i++){
+        int  len = words[i].length();
+        if (len > max){
+            max = len;
+        }
+        if (len < min){
+            min = len;
+        }
+     }
+        System.out.println(max-min);
     }
+        
 }

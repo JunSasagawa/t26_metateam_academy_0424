5cc644fd783e4668227c9e7b57315a4fcf29eed9
Masayuki-FujiiMT <masayuki.fujii@metateam.co.jp>
2026-04-24 11:48:13 +0900

    Training.javaの実装

--- src/main/java/jp/co/metateam/training/Training.java
+++ src/main/java/jp/co/metateam/training/Training.java
@@ -3,9 +3,39 @@ package jp.co.metateam.training;
 public class Training {
     public static void main(String[] args) {
         // 問1
-
+        int Before_Weight =10;
+        int N=30;
+        int increase_weight=N-Before_Weight;
+        System.out.println(increase_weight);
+        
         // 問2
+        int n=50;
+        int m=3;
+        int p=10;
+        int remaining_cards=n-m*p;
+        System.out.println(remaining_cards);
 
         // 問3
+          String[] Messages = {"book","pen","car","computer"};
+          int Max_length = Messages[0].length();
+          int min_length = Messages[0].length();
+
+          for(int i =0;
+            i < Messages.length; i++)
+            {int length = Messages[i].length();
+                if(length > Max_length){
+                   Max_length = length;
+
+    
+            }
+            if (length < min_length) {
+                min_length = length;
+            }
+        }
+        int Variation = Max_length - min_length;
+        System.out.println(Variation);
+          
+
+
     }
 }

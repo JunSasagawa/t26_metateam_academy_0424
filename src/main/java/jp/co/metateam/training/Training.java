66d1eaa99628fc8b922d7ea6f9d2c2ba9a1a4121
Sayu-Kasuya <sayu.kasuya@metateam.co.jp>
2026-04-24 11:54:48 +0900

    Training.javaの実装

--- src/main/java/jp/co/metateam/training/Training.java
+++ src/main/java/jp/co/metateam/training/Training.java
@@ -3,9 +3,36 @@ package jp.co.metateam.training;
 public class Training {
     public static void main(String[] args) {
         // 問1
+        int beforeweight = 10;//1年前の体重
+        int nowweight = 30;//現在の体重
+        int weight = nowweight - beforeweight;
+        System.out.println(weight);
+    
+
+
 
         // 問2
+        int n = 50;
+        int m = 3;
+        int p = 10;
+        int result = n-(m*p);
+        System.out.println(result);
 
         // 問3
+        String[]messages ={"book","pen","car","computer"};
+        int max = messages[0].length();
+        int min = messages[0].length();
+
+        for(int i = 0; i < messages.length; i++){
+        int length = messages[i].length();
+        if(length > max){
+        max = length;
+        }
+        if(length < min){
+            min = length;
+        }
+        }
+        int varietion = max - min;
+        System.out.println(varietion);
     }
 }
